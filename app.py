b3ac110be48792a8a0b9daa031984e837e380126
SwarajXO <purarkarswaraj@gmail.com>
2025-10-19 11:59:12 +0530

    Messaged Updated Again

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return jsonify({"message": "Hello Kunal, I have Deployed my first Flask API!"})
+    return jsonify({"message": "Hello, I have Deployed my first Flask API!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
